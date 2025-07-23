0e3ad96ba2862cd68ea2a12cbe753a198b61d3af
quigua <elvis.h72@yahoo.com>
2025-07-23 17:08:27 -0400

    Actualizando adaptador netlify para funciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,7 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'static',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  vite: {
-    server: {
-      proxy: {
-        '/api/': {
-          target: 'http://localhost:9999/.netlify/functions/',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\//, ''),
-        },
-      },
-    },
-  },
+  adapter: netlify(),
 });
